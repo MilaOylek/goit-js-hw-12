f2f5c071b0eba43298b8258d4b6af61f854df81a
MilaOylek <oylekmila@gmail.com>
2025-04-04 11:40:52 +0300

    change html js files

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,26 +1,3 @@
-// import axios from 'axios';
-
-// const BASE_URL = 'https://pixabay.com/api/';
-// const API_KEY = '49372169-96077d899f4beec9c9139b15f';
-
-// export function getImagesByQuery(query) {
-//   const params = {
-//     key: API_KEY,
-//     q: query,
-//     image_type: 'photo',
-//     orientation: 'horizontal',
-//     safesearch: true,
-//   };
-
-//   return axios
-//     .get(BASE_URL, { params })
-//     .then(response => response.data)
-//     .catch(error => {
-//       console.error('Error fetching images:', error);
-//       throw error;
-//     });
-// }
-
 import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
